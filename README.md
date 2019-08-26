7ae056b246fe0bcfd370c2546ef5cddb0ce416a4
Alex <revilosiz@gmail.com>
2019-08-26 17:18:20 +0300

    Added search ads

---
+++ README.md
@@ -0,0 +1,72 @@
+# UserAcquisition
+
+[![CI Status](https://img.shields.io/travis/Mikhail Verenich/UserAcquisition.svg?style=flat)](https://travis-ci.org/Mikhail Verenich/UserAcquisition)
+[![Version](https://img.shields.io/cocoapods/v/UserAcquisition.svg?style=flat)](https://cocoapods.org/pods/UserAcquisition)
+[![License](https://img.shields.io/cocoapods/l/UserAcquisition.svg?style=flat)](https://cocoapods.org/pods/UserAcquisition)
+[![Platform](https://img.shields.io/cocoapods/p/UserAcquisition.svg?style=flat)](https://cocoapods.org/pods/UserAcquisition)
+
+## Example
+
+To run the example project, clone the repo, and run `pod install` from the Example directory first.
+
+## Requirements
+
+## Installation
+
+UserAcquisition is available through [CocoaPods](https://cocoapods.org). To install
+it, simply add the following line to your Podfile:
+
+```ruby
+pod 'UserAcquisition', :git => "https://github.com/97mik/UserAcquisition.git"
+```
+
+## Usage
+In didFinishLaunchingWithOptions method:
+```swift
+UserAcquisition.shared.configure(withAPIKey: "APIKey")
+```
+After successful purchase:
+```swift
+UserAcquisition.shared.logPurchase(of: product)
+```
+Add AppsFlyer:
+```swift
+import AppsFlyerLib
+
+extension AppDelegate: AppsFlyerTrackerDelegate {
+    func onConversionDataReceived(_ installData: [AnyHashable : Any]!) {
+        if let data = installData as? [String: Any] {
+            UserAcquisition.shared.conversionInfo.setAppsFlyerData(data)
+            UserAcquisition.shared.conversionInfo.appsFlyerId = AppsFlyerTracker.shared().getAppsFlyerUID() ?? ""
+        }
+    }
+}
+```
+Add Adjust:
+```swift
+import Adjust
+
+extension AppDelegate: AdjustDelegate {
+    func adjustAttributionChanged(_ attribution: ADJAttribution?) {
+        if let data = attribution?.dictionary() {
+            UserAcquisition.shared.conversionInfo.setAdjustData(data)
+        }
+    }
+}
+```
+Add YandexMetrica:
+```swift
+import YandexMobileMetrica
+
+YMMYandexMetrica.requestAppMetricaDeviceID(withCompletionQueue: .main) { [unowned self] id, error in
+    UserAcquisition.shared.conversionInfo.appmetricaId = id ?? ""
+}
+```
+
+## Author
+
+Mikhail Verenich, 97mik@mail.ru
+
+## License
+
+UserAcquisition is available under the MIT license. See the LICENSE file for more info.
---
+++ UserAcquisition/Classes/UserAcquisition.swift
@@ -0,0 +1,204 @@
+//
+//  LTV.swift
+//  LTV
+//
+//  Created by Igor Khmurets on 9/24/17.
+//  Copyright © 2017 dp. All rights reserved.
+//
+
+import Foundation
+import AdSupport
+import StoreKit
+import SwiftyStoreKit
+
+public class UserAcquisition: NSObject {
+
+    public static let shared = UserAcquisition()
+    
+    public var conversionInfo = UserAcquisition.Info()
+    
+    private var APIKey = ""
+
+    public func configure(withAPIKey APIKey: String) {
+        self.APIKey = APIKey
+    }
+    
+    public func logPurchase(of product: SKProduct) {
+        var receipt: String?
+        let group = DispatchGroup()
+        group.enter()
+        SwiftyStoreKit.fetchReceipt(forceRefresh: false) { result in
+            switch result {
+            case .success(let receiptData):
+                receipt = receiptData.base64EncodedString()
+            case .error(let error):
+                print(error)
+                break
+            }
+            group.leave()
+        }
+        group.notify(queue: .global()) {
+            if let receipt = receipt {
+                self.logPurchase(info: self.conversionInfo, product: product, receipt: receipt)
+            }
+        }
+    }
+
+    public func setExtraValue(_ value: Any, forKey key: String) {
+        conversionInfo.extra[key] = value
+    }
+
+    private func logPurchase(info: Info, product: SKProduct, receipt: String) {
+        var acquisitionSource: String {
+            switch info.acquisitionSource {
+            case .facebook:
+                return "Facebook"
+            case .searchAds:
+                return "Search Ads"
+            case .organic:
+                return "Organic"
+            case let .custom(source):
+                return source
+            }
+        }
+        var afi: String? {
+            if ASIdentifierManager.shared().isAdvertisingTrackingEnabled {
+                return ASIdentifierManager.shared().advertisingIdentifier.uuidString
+            } else {
+                return nil
+            }
+        }
+        let iap: [String: Any] = [
+            "product_id": product.productIdentifier,
+            "price": product.price.stringValue,
+            "currency": product.priceLocale.currencyCode ?? "",
+        ]
+        var extra: [String: Any] = [
+            "acquisition_source": acquisitionSource,
+            "acquisition_date": Int(info.acquisitionDate.timeIntervalSince1970),
+            "ad_campaign": info.adCampaign,
+            "ad_group": info.adGroup,
+            "ad_creative": info.adCreative,
+            "vendor_id": UIDevice.current.identifierForVendor?.uuidString ?? "",
+            "appsflyer_id": info.appsFlyerId,
+            "appmetrica_device_id": info.appmetricaId,
+            "adjust_raw": info.adjustRaw,
+            "appsflyer_raw": info.appsFlyerRaw,
+            "searchads_raw": info.searchAdsRaw
+        ]
+        for (field, value) in info.extra {
+            extra[field] = value
+        }
+        let params: [String: Any?] = [
+            "bundle_id": Bundle.main.bundleIdentifier ?? "",
+            "afi": afi,
+            "receipt": receipt,
+            "iap": iap,
+            "country": product.priceLocale.regionCode ?? "",
+            "extra": extra,
+            "api_key": APIKey
+        ]
+        var request = URLRequest(url: URL(string: "https://api.subr.app/v2/receipt")!)
+        request.httpMethod = "POST"
+        request.httpBody = try! JSONSerialization.data(withJSONObject: params)
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        URLSession.shared.dataTask(with: request) { data, resp, error in
+            guard let data = data, let json = (try? JSONSerialization.jsonObject(with: data)) as? [String: Any] else {
+                return
+            }
+            print(json)
+        }.resume()
+    }
+}
+
+extension UserAcquisition {
+    public var isConversionUser: Bool {
+        if case .organic = UserAcquisition.shared.conversionInfo.acquisitionSource {
+            return false
+        } else {
+            return true
+        }
+    }
+}
+
+extension UserAcquisition {
+    public struct Info {
+        enum AcquisitionSource {
+            case organic, facebook, searchAds, custom(String)
+        }
+        var userId: String?
+        var acquisitionSource: AcquisitionSource = .organic
+        var acquisitionDate = Date()
+        var adCampaign = ""
+        var adGroup = ""
+        var adCreative = ""
+        public var appsFlyerId = ""
+        public var appmetricaId = ""
+        var adjustRaw = ""
+        var appsFlyerRaw = ""
+        var searchAdsRaw = ""
+        var extra = [String: Any]()
+
+        public mutating func setAppsFlyerData(_ appsFlyerData: [String: Any]) {
+            if let jsonData = try? JSONSerialization.data(withJSONObject: appsFlyerData, options: .prettyPrinted) {
+                self.appsFlyerRaw = String(data: jsonData, encoding: .utf8) ?? ""
+            }
+            
+            let status = appsFlyerData["af_status"] as? String ?? ""
+            let source = appsFlyerData["media_source"] as? String ?? ""
+            let campaign = appsFlyerData["campaign"] as? String ?? ""
+            let campaignId = appsFlyerData["campaign_id"] as? String ?? ""
+            let adSet = appsFlyerData["adset"] as? String ?? ""
+            let adSetId = appsFlyerData["adset_id"] as? String ?? ""
+            let ad = appsFlyerData["ad"] as? String ?? ""
+            let adId = appsFlyerData["ad_id"] as? String ?? ""
+            print(appsFlyerData)
+            var acquisitionSource: UserAcquisition.Info.AcquisitionSource {
+                switch status {
+                case "Non-organic":
+                    switch source {
+                    case "Facebook Ads":
+                        return .facebook
+                    default:
+                        return .custom(source)
+                    }
+                case "Organic":
+                    return .organic
+                default:
+                    return .custom("Undefined")
+                }
+            }
+            var acquisitionDate: Date {
+                let formatter = DateFormatter()
+                formatter.dateFormat = "yyyy-MM-DD mm:HH:ss.SSS"
+                return formatter.date(from: appsFlyerData["install_time"] as? String ?? "") ?? Date()
+            }
+            func merged(_ str1: String, _ str2: String) -> String {
+                if str2 == "" {
+                    return str1
+                } else {
+                    return "\(str1) (\(str2))"
+                }
+            }
+            self.acquisitionSource = acquisitionSource
+            self.acquisitionDate = acquisitionDate
+            self.adCampaign = merged(campaign, campaignId)
+            self.adGroup = merged(adSet, adSetId)
+            self.adCreative = merged(ad, adId)
+        }
+        
+        public mutating func setAdjustData(_ adjustData: [String: Any]) {
+            if let jsonData = try? JSONSerialization.data(withJSONObject: adjustData, options: .prettyPrinted) {
+                self.adjustRaw = String(data: jsonData, encoding: .utf8) ?? ""
+            }
+        }
+        
+        public mutating func setSearchAds(_ searchAdsData: [String: Any]){
+            
+            if let jsonData = try? JSONSerialization.data(withJSONObject: searchAdsData, options: .prettyPrinted){
+                self.searchAdsRaw = String(data: jsonData, encoding: .utf8) ?? ""
+            }
+        }
+    }
+}
